c2b7100a3499abe2c5803fd97a2352f249260256
khushboo yadav <mc21064@zealeducation.com>
2024-01-16 15:27:10 +0530

    first Commit

---
+++ src/test/java/com/springboot/StudentApicrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentApicrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
