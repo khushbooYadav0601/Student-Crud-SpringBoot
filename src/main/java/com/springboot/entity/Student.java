d64aacfdf3374f3372f0ef8c8bed8d2ee6a9a4db
Khushboo Yadav <110219995+khushbooYadav0601@users.noreply.github.com>
2024-01-16 21:15:15 +0530

    Update Student.java
    Entity Class All Done

--- src/main/java/com/springboot/entity/Student.java
+++ src/main/java/com/springboot/entity/Student.java
@@ -12,58 +12,54 @@ public class Student {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int sid;
-	private String sname;
-	private int sage;
-	private String sClass;
-	private String saddress;
+	private int s_id;
+	private String s_name;
+	private int s_age;
+	private String s_grade;
+	private String s_address;
+	public int getS_id() {
+		return s_id;
+	}
+	public void setS_id(int s_id) {
+		this.s_id = s_id;
+	}
+	public String getS_name() {
+		return s_name;
+	}
+	public void setS_name(String s_name) {
+		this.s_name = s_name;
+	}
+	public int getS_age() {
+		return s_age;
+	}
+	public void setS_age(int s_age) {
+		this.s_age = s_age;
+	}
+	public String getS_grade() {
+		return s_grade;
+	}
+	public void setS_grade(String s_grade) {
+		this.s_grade = s_grade;
+	}
+	public String getS_address() {
+		return s_address;
+	}
+	public void setS_address(String s_address) {
+		this.s_address = s_address;
+	}
 	
-	public int getSid() {
-		return sid;
-	}
-	public void setSid(int sid) {
-		this.sid = sid;
-	}
-	public String getSname() {
-		return sname;
-	}
-	public void setSname(String sname) {
-		this.sname = sname;
-	}
-	public int getSage() {
-		return sage;
-	}
-	public void setSage(int sage) {
-		this.sage = sage;
-	}
-	public String getsClass() {
-		return sClass;
-	}
-	public void setsClass(String sClass) {
-		this.sClass = sClass;
-	}
-	public String getSaddress() {
-		return saddress;
-	}
-	public void setSaddress(String saddress) {
-		this.saddress = saddress;
-	}
-	public Student(int sid, String sname, int sage, String sClass, String saddress) {
+	public Student(int s_id, String s_name, int s_age, String s_grade, String s_address) {
 		super();
-		this.sid = sid;
-		this.sname = sname;
-		this.sage = sage;
-		this.sClass = sClass;
-		this.saddress = saddress;
+		this.s_id = s_id;
+		this.s_name = s_name;
+		this.s_age = s_age;
+		this.s_grade = s_grade;
+		this.s_address = s_address;
 	}
 	
 	public Student() {
 		
 	}
-	@Override
-	public String toString() {
-		return "Student [sid=" + sid + ", sname=" + sname + ", sage=" + sage + ", sClass=" + sClass + ", saddress="
-				+ saddress + "]";
-	}
 	
 }
+
