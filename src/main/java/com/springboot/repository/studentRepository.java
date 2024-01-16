a41ebffb3f0d2daa28f76ef65d041f0263f9ca44
Khushboo Yadav <110219995+khushbooYadav0601@users.noreply.github.com>
2024-01-16 22:40:32 +0530

    Update studentRepository.java, Using Custom Query to fetch data by name

--- src/main/java/com/springboot/repository/studentRepository.java
+++ src/main/java/com/springboot/repository/studentRepository.java
@@ -1,9 +1,16 @@
 package com.springboot.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import com.springboot.entity.Student;
 
 public interface studentRepository extends JpaRepository<Student, Integer>{
+
+	
+	@Query("SELECT c FROM Student c WHERE c.s_name = :s_name")
+	Optional<Student> findByName(@Param("s_name") String s_name);
 
 }
